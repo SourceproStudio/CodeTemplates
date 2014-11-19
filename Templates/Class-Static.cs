0e47703afd7a506d677ef62196b0ac9166feeed1
王 玉才 <180261899@qq.com>
2014-11-19 15:00:49 +0800

    新增代码模板。

---
+++ Templates/Class-Static.cs
@@ -0,0 +1,54 @@
+﻿#region README
+
+/*
+ * README
+ * 
+ * Current Thread User : $userdomain$\$username$
+ * Machine Name : $machinename$
+ * Visual Studio : Microsoft Visual Studio 2010 Ultimate Edition
+ * Create Time : $time$
+ * Common Language Runtime : $clrversion$
+ * Minimum .Net Framework Version : $targetframeworkversion$
+ * 
+ * SourcePro Studio $year$
+ * Project Url : https://github.com/SourceproStudio/CodeTemplates
+ * Home Page Url : https://github.com/SourceproStudio
+ * E-mail Address : MasterDuner@yeah.net or Yucai.Wang-Public@outlook.com
+ * QQ : 180261899
+ */
+
+#endregion
+
+using System;
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// <para>
+    /// Description
+    /// </para>
+    /// <para>
+    /// Namespace : <see cref="$rootnamespace$"/>
+    /// </para>
+    /// <para>
+    /// Type : <see cref="$safeitemname$"/>
+    /// </para>
+    /// <para>
+    /// The minimum .Net Framework version requirements : $targetframeworkversion$
+    /// </para>
+    /// <para>
+    /// <see cref="$safeitemname$"/> is a static type !
+    /// </para>
+    /// </summary>
+    /// <remarks>
+    /// <see cref="$safeitemname$"/> is a static type !
+    /// </remarks>
+    /// <seealso cref="$rootnamespace$"/>
+    public static class $safeitemname$
+    {
+    }
+}
+
+/*
+ * Copyright © $year$ Wang Yucai. All rights reserved.
+ */
---
+++ Templates/Enumeration-WithFlags.cs
@@ -0,0 +1,49 @@
+﻿#region README
+
+/*
+ * README
+ * 
+ * Current Thread User : $userdomain$\$username$
+ * Machine Name : $machinename$
+ * Visual Studio : Microsoft Visual Studio 2010 Ultimate Edition
+ * Create Time : $time$
+ * Common Language Runtime : $clrversion$
+ * Minimum .Net Framework Version : $targetframeworkversion$
+ * 
+ * SourcePro Studio $year$
+ * Project Url : https://github.com/SourceproStudio/CodeTemplates
+ * Home Page Url : https://github.com/SourceproStudio
+ * E-mail Address : MasterDuner@yeah.net or Yucai.Wang-Public@outlook.com
+ * QQ : 180261899
+ */
+
+#endregion
+
+using System;
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// <para>
+    /// Description
+    /// </para>
+    /// <para>
+    /// Namespace : <see cref="$rootnamespace$"/>
+    /// </para>
+    /// <para>
+    /// Type : <see cref="$safeitemname$"/>
+    /// </para>
+    /// <para>
+    /// The minimum .Net Framework version requirements : $targetframeworkversion$
+    /// </para>
+    /// </summary>
+    /// <seealso cref="$rootnamespace$"/>
+    [Serializable, Flags]
+    public enum $safeitemname$
+    {
+    }
+}
+
+/*
+ * Copyright © $year$ Wang Yucai. All rights reserved.
+ */
